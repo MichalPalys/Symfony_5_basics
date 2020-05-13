b40514beafec769af8ac80fbca1be8f182554e8e
Michał Pałys <michael.palys@wp.pl>
2020-05-13 10:14:37 +0200

    add annotations for homepage() function

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route(path="/")
+     *
+     * @return Response
+     */
     public function homepage()
     {
         return new Response("It`s work!",200);

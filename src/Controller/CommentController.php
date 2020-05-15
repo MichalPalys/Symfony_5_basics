e1a835502016e8ef4bf8d3a0e91230fc3868515c
Michał Pałys <michael.palys@wp.pl>
2020-05-15 09:30:44 +0200

    add route annotations to commentVote() function

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
+    /**
+     * @Route(path="/comments/{id}/vote/{direction}")
+     */
     public function commentVote()
     {
-        
+
     }
 }

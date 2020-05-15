9f8bb1ea86cb6c1fb73b761b87b6be7a8ac57edb
Michał Pałys <michael.palys@wp.pl>
2020-05-15 09:50:13 +0200

    complet body of commetVote() function

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,15 +5,31 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
     /**
      * @Route(path="/comments/{id}/vote/{direction}")
+     *
+     * @param $id
+     * @param string $direction
+     *
+     * @return JsonResponse
      */
-    public function commentVote()
+    public function commentVote($id, string $direction)
     {
+        //todo later this will be based on database
 
+        if ($direction === 'up')
+        {
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $currentVoteCount = rand(0, 5);
+        }
+
+
+        return $this->json(['votes' => $currentVoteCount]);
     }
 }

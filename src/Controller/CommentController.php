94795e689b302ae592c8f2db8d31e1fa28d8ff5f
Michał Pałys <michael.palys@wp.pl>
2020-05-15 13:05:55 +0200

    add route method, wildcard param regExp and log info

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,21 +12,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route(path="/comments/{id}/vote/{direction}")
+     * @Route(path="/comments/{id}/vote/{direction<up|down>}", methods="POST")
      *
      * @param $id
      * @param string $direction
      *
      * @return JsonResponse
      */
-    public function commentVote($id, string $direction)
+    public function commentVote($id, string $direction, LoggerInterface $logger)
     {
         //todo later this will be based on database
 
         if ($direction === 'up')
         {
+            $logger->info('Voting Up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting Down!');
             $currentVoteCount = rand(0, 5);
         }
 

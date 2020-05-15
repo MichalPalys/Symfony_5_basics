99664c0bd2928e08acd84144480e8602436c365b
Michał Pałys <michael.palys@wp.pl>
2020-05-15 11:45:30 +0200

    refactor js script in show.js

--- public/js/question_show.js
+++ public/js/question_show.js
@@ -7,8 +7,13 @@ $container.find('a').on('click', function (e) {
     e.preventDefault();
     var $link = $(e.currentTarget);
 
+    let currentLocation = location.href;
+    let end_text = 'public';
+    let absolute_root_path = currentLocation.substring(0, currentLocation.indexOf(end_text) + end_text.length);
+
+
     $.ajax({
-        url: 'comments/10/vote/' + $link.data('direction'),
+        url: absolute_root_path + '/comments/10/vote/' + $link.data('direction'),
         method: 'POST'
     }).then(function (data) {
         $container.find('.js-vote-total').text(data.votes);
